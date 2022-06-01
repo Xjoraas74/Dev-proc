9b7c2c8ac38afbc04f8bb4296390154bb013caff
xidios <47945350+xidios@users.noreply.github.com>
2022-06-02 02:15:37 +0700

    Пока что реализована только часть БЛ 2.0

---
+++ Dev-proc/Migrations/20220601184754_FileAddedForUser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dev_proc.Migrations
+{
+    public partial class FileAddedForUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ResumeId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ResumeId",
+                table: "AspNetUsers",
+                column: "ResumeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Files_ResumeId",
+                table: "AspNetUsers",
+                column: "ResumeId",
+                principalTable: "Files",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Files_ResumeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ResumeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Dev-proc/Models/ViewModels/ResumeFileViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Dev_proc.Models.ViewModels
+{
+    public class ResumeFileViewModel
+    {
+        public Guid StudentId { get; set; }
+        public IFormFile Resume { get; set; }
+    }
+}
