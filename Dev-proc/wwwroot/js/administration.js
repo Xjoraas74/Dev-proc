ff78e7b02480cb1e754ed12a464aa8e2fa4d5445
Xjoraas74 <xjoraas@gmail.com>
2022-06-03 20:25:28 +0700

    Commit TSU front files

---
+++ Dev-proc/wwwroot/js/administration.js
@@ -0,0 +1,65 @@
+﻿$(document).ready(function () {
+    var activeRequest = null;
+    var timer = null;
+    $("#newUser").typeahead(null,
+        {
+            source: function (query) {
+                if (query.length < 4) {
+                    return null;
+                }
+
+                if (timer !== null) {
+                    clearTimeout(timer);
+                }
+
+                if (activeRequest !== null) {
+                    activeRequest.abort();
+                    activeRequest = null;
+                }
+
+                timer = setTimeout(function () {
+                    activeRequest = $.ajax("/administration/find_user/" + query,
+                        {
+                            type: "GET",
+                            success: function (data) {
+                                $('#usersFindResult').html(data);
+                            }
+                        });
+                },
+                    500);
+
+                return activeRequest;
+            }
+        });
+
+    $("#userName").typeahead(null,
+        {
+            source: function (query) {
+                if (query.length < 4) {
+                    return null;
+                }
+
+                if (timer !== null) {
+                    clearTimeout(timer);
+                }
+
+                if (activeRequest !== null) {
+                    activeRequest.abort();
+                    activeRequest = null;
+                }
+
+                timer = setTimeout(function () {
+                    activeRequest = $.ajax("/administration/find_new_user/" + query,
+                        {
+                            type: "GET",
+                            success: function (data) {
+                                $('#usersFindResult').html(data);
+                            }
+                        });
+                },
+                    500);
+
+                return activeRequest;
+            }
+        });
+});
